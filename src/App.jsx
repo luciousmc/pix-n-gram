c96984a325752071741f7fdc3faed21e3e014615
Marlon Clay <luciousmc@icloud.com>
2021-11-02 14:12:02 -0700

    adds react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Login = lazy(() => import('./pages/Login'));
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Pix an Gram!</h1>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path='/login' component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
