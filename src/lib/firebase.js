753e2fbdcfba43274a6f8acdbc3c07f3a4d985b7
Marlon Clay <luciousmc@icloud.com>
2021-11-02 13:52:32 -0700

    firestore setup

---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, FieldValue } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// import seed file
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAT9TGt6-pthF6JG-ps9GAKI3rBb0hIE6Q',
+  authDomain: 'pix-n-gram.firebaseapp.com',
+  projectId: 'pix-n-gram',
+  storageBucket: 'pix-n-gram.appspot.com',
+  messagingSenderId: '348192370164',
+  appId: '1:348192370164:web:9eba9f95492d3e32e19da1',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+// call the seed file
+// seedDatabase()
+
+export { firebaseApp, db, auth, FieldValue };
--- src/seed.js
+++ src/seed.js
@@ -1,13 +1,16 @@
-export function seedDatabase(firebase) {
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from './lib/firebase';
+
+export function seedDatabase() {
   const users = [
     {
-      userId: 'NvPY9M9MzFTARQ6M816YAzDJxZ72',
-      username: 'karl',
-      fullName: 'Karl Hadwen',
-      emailAddress: 'karlhadwen@gmail.com',
+      userId: 'zeujiMw3JhR80Zsmggo7kD9zgs02',
+      username: 'Marlon',
+      fullName: 'Marlon Clay',
+      emailAddress: 'luciousmc@icloud.com',
       following: ['2'],
       followers: ['2', '3', '4'],
-      dateCreated: Date.now(),
+      dateCreated: serverTimestamp,
     },
     {
       userId: '2',
@@ -15,8 +18,8 @@ export function seedDatabase(firebase) {
       fullName: 'Raffaello Sanzio da Urbino',
       emailAddress: 'raphael@sanzio.com',
       following: [],
-      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
-      dateCreated: Date.now(),
+      followers: ['zeujiMw3JhR80Zsmggo7kD9zgs02'],
+      dateCreated: serverTimestamp,
     },
     {
       userId: '3',
@@ -24,8 +27,8 @@ export function seedDatabase(firebase) {
       fullName: 'Salvador Dalí',
       emailAddress: 'salvador@dali.com',
       following: [],
-      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
-      dateCreated: Date.now(),
+      followers: ['zeujiMw3JhR80Zsmggo7kD9zgs02'],
+      dateCreated: serverTimestamp,
     },
     {
       userId: '4',
@@ -33,40 +36,37 @@ export function seedDatabase(firebase) {
       fullName: 'George Orwell',
       emailAddress: 'george@orwell.com',
       following: [],
-      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
-      dateCreated: Date.now(),
+      followers: ['zeujiMw3JhR80Zsmggo7kD9zgs02'],
+      dateCreated: serverTimestamp,
     },
   ];
 
   // eslint-disable-next-line prefer-const
   for (let k = 0; k < users.length; k++) {
-    firebase.firestore().collection('users').add(users[k]);
+    addDoc(collection(db, 'users'), users[k]);
   }
 
   // eslint-disable-next-line prefer-const
   for (let i = 1; i <= 5; ++i) {
-    firebase
-      .firestore()
-      .collection('photos')
-      .add({
-        photoId: i,
-        userId: '2',
-        imageSrc: `/images/users/raphael/${i}.jpg`,
-        caption: 'Saint George and the Dragon',
-        likes: [],
-        comments: [
-          {
-            displayName: 'dali',
-            comment: 'Love this place, looks like my animal farm!',
-          },
-          {
-            displayName: 'orwell',
-            comment: 'Would you mind if I used this picture?',
-          },
-        ],
-        userLatitude: '40.7128°',
-        userLongitude: '74.0060°',
-        dateCreated: Date.now(),
-      });
+    addDoc(collection(db, 'photos'), {
+      photoId: i,
+      userId: '2',
+      imageSrc: `/images/users/raphael/${i}.jpg`,
+      caption: 'Saint George and the Dragon',
+      likes: [],
+      comments: [
+        {
+          displayName: 'dali',
+          comment: 'Love this place, looks like my animal farm!',
+        },
+        {
+          displayName: 'orwell',
+          comment: 'Would you mind if I used this picture?',
+        },
+      ],
+      userLatitude: '40.7128°',
+      userLongitude: '74.0060°',
+      dateCreated: serverTimestamp,
+    });
   }
 }
